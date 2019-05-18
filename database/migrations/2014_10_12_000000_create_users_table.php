6416de542a297b6c50de06f0a636269c500196e7
Hein Thanth <official.heinthanth@gmail.com>
2019-05-18 21:19:39 +0630

    Fixed User Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo')->default(null);
+            $table->string('photo')->nullable()->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
