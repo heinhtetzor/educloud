2b3f4a4010905ef584d31cb03ccf43e55e4d7a50
Hein Thanth <official.heinthanth@gmail.com>
2019-05-19 14:58:01 +0630

    Delete Unnecessary JS code

--- public/js/common.js
+++ public/js/common.js
@@ -1,19 +1,9 @@
-"use strict";
-
-var lat, long;
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(updateLocation);
       } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
       }
-}
-
-function showPosition(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    
 }
 
 $(document).ready(function() {
