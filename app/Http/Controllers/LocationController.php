dcab21a70674424278265b9cba3dbf832524ab58
Hein Thanth <official.heinthanth@gmail.com>
2019-05-19 15:12:21 +0630

    Add Update function in LocationController

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -9,11 +9,30 @@ use Auth;
 class LocationController extends Controller
 {
     public function update(Request $request) {
-        /* DB::table('location')->insert([
-            'user_id' => Auth::user()->id,
-            'latitude' => $request->lat,
-            'longitude' => $request->long,
-        ]); */
-        echo "lat = ".$request->input("lat")." && long = ".$request->input("long");
+        $user = Auth::user()->id;
+        if(count(DB::table('location')->where('user_id', $user)->get()) == 0) {
+            DB::table('location')->insert([
+                'user_id' => $user,
+                'latitude' => $request->input("lat"),
+                'longitude' => $request->input("long"),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        } else {
+
+            // location is update in every 24 Hours
+
+            $lastRecord = DB::table('location')->where('user_id', $user)->first();
+            if((time() - strtotime($lastRecord->updated_at)) > 129600){
+                DB::table('location')->where('user_id', $user)->update([
+                    'latitude' => $request->input("lat"),
+                    'longitude' => $request->input("long"),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ]);
+            } else {
+                
+            }
+            
+        }
     }
 }
